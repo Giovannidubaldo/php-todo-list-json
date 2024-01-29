f6175432523cd9dcf3e1eab08ef70d589c6be523
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-29 16:08:07 +0100

    Added function axios

--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            apiUrl: 'server.php',
+            city: [],
         }
     },
     mounted() {
-
+        this.getListCity();
     },
     methods: {
-
+        getListCity() {
+            axios.get(this.apiUrl).then((response) => {
+                this.city = response.data;
+            })
+        }
     },
 }).mount('#app')
