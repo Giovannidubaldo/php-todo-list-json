58c3d881757e75f9e197ae24a303afb49041f683
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2024-01-29 16:18:13 +0100

    Recovered json in server.php

--- server.php
+++ server.php
@@ -1,3 +1,7 @@
 <?php
-    
+    // Recupero l'array di oggetti nel file json
+    $list_of_city = file_get_contents('todo-list.json');
+
+    header('Content-type: application/json');
+    echo $list_of_city;
 ?>
